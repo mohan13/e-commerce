b9d07004a30ffe5860df81a5d7b0b75d4b305805
Mohan <mohangrg250@gmail.com>
2022-02-05 23:33:15 +0545

    e-commerce 1.3

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const open = document.querySelector(".open")
 const list = document.querySelector('.nav-list')
-open.onclick = function(){
-    list.style.width ="100vw";
-    list.style.left="0";
-};
 document.querySelector(".close").onclick = function(){
     list.style.width ="0";
     list.style.left="-100%";
 }; 
+open.onclick = function(){
+    list.style.width ="100vw";
+    list.style.left="0";
+};
 
 const search = document.querySelector(".search")
 search.onclick = function(){
